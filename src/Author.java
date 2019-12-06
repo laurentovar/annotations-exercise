5516791937cbaa364a041f0683b83cf644907635
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-12-06 12:18:20 -0600

    easy fix

--- src/Author.java
+++ src/Author.java
@@ -3,7 +3,7 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-
+@SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
 
@@ -16,6 +16,7 @@ public class Author extends Person {
      * @deprecated Use publishedBooks instead
      */
     @Deprecated
+
     public List<String> getBooks() {
         return books;
     }
@@ -29,6 +30,7 @@ public class Author extends Person {
     }
 
 //    @Override
+
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
